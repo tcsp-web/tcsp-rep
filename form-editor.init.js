2cb78f76a939c473cbb5441c6f83ce3cd6d638fc
tcsp-web <thecheapestsmmpanels@gmail.com>
2024-12-16 19:53:21 +0530

    Add files via upload

---
+++ form-editor.init.js
@@ -0,0 +1,47 @@
+var quill = new Quill('#editor1', {
+	modules: {
+		toolbar: [
+			[{ header: [1, 2, 3, 4, 5, 6, false] }],
+			['bold', 'italic', 'underline'],
+			['image', 'video'],
+			['align', { align: 'center' }, { align: 'right' }],			
+			['strike', 'link' ],
+			[{ color: ['red', 'aqua', 'blue', 'teal', 'navy', 'purple', 'yellow', 'lime', 'fuchsia', 'white', 'silver', 'gray', 'black', 'maroon', 'green', 'orange', 'cyan', 'olive'] }],
+			[{ background: ['red', 'aqua', 'blue', 'teal', 'navy', 'purple', 'yellow', 'lime', 'fuchsia', 'white', 'silver', 'gray', 'black', 'maroon', 'green', 'orange', 'cyan', 'olive'] }]
+		]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow',
+});
+
+var quill = new Quill('#editor2', {
+	modules: {
+		toolbar: [
+			[{ header: [1, 2, 3, 4, 5, 6, false] }],
+			['bold', 'italic', 'underline'],
+			['image', 'video'],
+			['align', { align: 'center' }, { align: 'right' }],			
+			['strike', 'link' ],
+			[{ color: ['red', 'aqua', 'blue', 'teal', 'navy', 'purple', 'yellow', 'lime', 'fuchsia', 'white', 'silver', 'gray', 'black', 'maroon', 'green', 'orange', 'cyan', 'olive'] }],
+			[{ background: ['red', 'aqua', 'blue', 'teal', 'navy', 'purple', 'yellow', 'lime', 'fuchsia', 'white', 'silver', 'gray', 'black', 'maroon', 'green', 'orange', 'cyan', 'olive'] }]
+		]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow',
+});
+
+var quill = new Quill('#editor3', {
+	modules: {
+		toolbar: [
+			[{ header: [1, 2, 3, 4, 5, 6, false] }],
+			['bold', 'italic', 'underline'],
+			['image', 'video'],
+			['align', { align: 'center' }, { align: 'right' }],			
+			['strike', 'link' ],
+			[{ color: ['red', 'aqua', 'blue', 'teal', 'navy', 'purple', 'yellow', 'lime', 'fuchsia', 'white', 'silver', 'gray', 'black', 'maroon', 'green', 'orange', 'cyan', 'olive'] }],
+			[{ background: ['red', 'aqua', 'blue', 'teal', 'navy', 'purple', 'yellow', 'lime', 'fuchsia', 'white', 'silver', 'gray', 'black', 'maroon', 'green', 'orange', 'cyan', 'olive'] }]
+		]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow',
+});
